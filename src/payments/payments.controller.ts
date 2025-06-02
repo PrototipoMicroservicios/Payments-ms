4b83aede63913d7e7dea2d03c5b48e30664c1825
RicardoTipan <ricardojaviertipan@gmail.com>
2025-06-01 21:54:04 -0500

    Creacion de endpoints en Payments-ms

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -1,7 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
 
 @Controller('payments')
 export class PaymentsController {
   constructor(private readonly paymentsService: PaymentsService) {}
+
+  @Post('create-payments-session')
+  createPaymentsSession(){
+    return 'createPaymentsSession';
+  }
+
+  @Get('success')
+  success(){
+    return{
+      ok: true,
+      message: 'Payment successful'
+    }
+  }
+
+  
+  @Get('cancel')
+  cancel(){
+    return{
+      ok: false,
+      message: 'Payment cancelled'
+    }
+  }
+
 }
